063ab19cc93680752b5de46daf910f1d803e7726
Banhawy <the.benhawy@gmail.com>
2017-07-14 17:56:08 -0500

    Added package.json and restructured file tree

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,69 @@
 var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var uglify = require('gulp-uglify');
+var useref = require('gulp-useref');
+var cssnano = require('gulp-cssnano');
+var runSequence = require('run-sequence');
 
-// Static server + watching scss/html files
-gulp.task('browser-sync', function() {
-    var files = [
-        '*.html',
-        'styles/css/**/*.css',
-        'js/**/*.js',
-        'styles/sass/**/*.scss'
-    ];
-    browserSync.init(files, {
+// Basic Gulp task syntax
+gulp.task('hello', function() {
+    console.log('Eshta Ya Me3allemm!');
+})
+
+// Development Tasks 
+// -----------------
+
+// Start browserSync server
+gulp.task('browserSync', function() {
+    browserSync({
         server: {
-            baseDir: "./"
+            baseDir: 'app'
         }
-    });
-});
+    })
+})
 
-// Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    gulp.src("styles/scss/*.scss")
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest("styles/css/"))
-        .pipe(browserSync.stream());
+    return gulp.src('app/styles/sass/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
+        .pipe(sass().on('error', sass.logError)) // Passes it through a gulp-sass, log errors to console
+        .pipe(gulp.dest('app/styles/css')) // Outputs it in the css folder
+        .pipe(browserSync.reload({ // Reloading with Browser Sync
+            stream: true
+        }));
+})
+
+// Watchers
+gulp.task('watch', function() {
+    gulp.watch('app//styles/sass/**/*.scss', ['sass']);
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload);
+})
+
+// Optimization Tasks 
+// ------------------
+
+// Optimizing CSS and JavaScript 
+gulp.task('useref', function() {
+
+    return gulp.src('app/*.html')
+        .pipe(useref())
+        .pipe(gulpIf('*.js', uglify()))
+        .pipe(gulpIf('*.css', cssnano()))
+        .pipe(gulp.dest('dist'));
 });
 
+// Build Sequences
+// ---------------
 
-gulp.task('default', ['browser-sync'], function() {
-    gulp.watch("styles/scss/**/*.scss", ['sass']).on('change', browserSync.reload);
-    gulp.watch("wiki/*.html").on('change', browserSync.reload);
-});
+gulp.task('default', function(callback) {
+    runSequence(['sass', 'browserSync'], 'watch',
+        callback
+    )
+})
+
+gulp.task('build', function(callback) {
+    runSequence(
+        ['sass', 'useref'],
+        callback
+    )
+})
