93c74fff059dff0053d407a3d7b9a7d7967ad3e4
Adham EL Banhawy <the.benhawy@gmail.com>
2017-07-16 23:37:15 -0500

    Added circles

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,3 +1,54 @@
+var AgileCircle = Circles.create({
+  id:                  'circles-1',
+  radius:              60,
+  value:               100,
+  maxValue:            100,
+  width:               10,
+  text:                function(){return '<h4>Agile<h4>';},
+  colors:              ['#97d5ec', '#028cd5'],
+  duration:            400,
+  wrpClass:            'circles-wrp',
+  textClass:           'circles-text',
+  valueStrokeClass:    'circles-valueStroke',
+  maxValueStrokeClass: 'circles-maxValueStroke',
+  styleWrapper:        true,
+  styleText:           true
+});
+
+var FontEndCircle = Circles.create({
+  id:                  'circles-2',
+  radius:              60,
+  value:               98,
+  maxValue:            100,
+  width:               10,
+  text:                function(){return '<h4>Front-End<h4>';},
+  colors:              ['#97d5ec', '#028cd5'],
+  duration:            400,
+  wrpClass:            'circles-wrp',
+  textClass:           'circles-text',
+  valueStrokeClass:    'circles-valueStroke',
+  maxValueStrokeClass: 'circles-maxValueStroke',
+  styleWrapper:        true,
+  styleText:           true
+});
+
+var BackEndCircle = Circles.create({
+  id:                  'circles-3',
+  radius:              60,
+  value:               50,
+  maxValue:            100,
+  width:               10,
+  text:                function(){return '<h4>Back-End<h4>';},
+  colors:              ['#97d5ec', '#028cd5'],
+  duration:            400,
+  wrpClass:            'circles-wrp',
+  textClass:           'circles-text',
+  valueStrokeClass:    'circles-valueStroke',
+  maxValueStrokeClass: 'circles-maxValueStroke',
+  styleWrapper:        true,
+  styleText:           true
+});
+
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
