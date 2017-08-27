f4dfde6da2220591fdfd5ab65e7546e368bc9e55
Banhawy <the.benhawy@gmail.com>
2017-08-27 02:00:09 -0500

    Increased type speed

--- docs/js/front-page.js
+++ docs/js/front-page.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
 
 var typed = new Typed('.monitor', {
     strings: ["a Web Developer.", "a Blogger.", "a Gamer.", "a technology enthusiast.", "a Software Engineer."],
-    typeSpeed: 65,
+    typeSpeed: 90,
     smartBackspace: true,
     backSpeed: 20,
     backDelay: 2000,
