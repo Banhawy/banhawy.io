5b5c42bf668bd7de67b20875a3e4fcd43b255d2d
Banhawy <the.benhawy@gmail.com>
2017-08-27 02:25:20 -0500

    Updated blog images rel paths

--- docs/python/blog_list.py
+++ docs/python/blog_list.py
@@ -10,9 +10,9 @@ document.close()
 
 blogpost.open_blog('Making a Python Web Crawler', content)
 
-personal_story = blog_object.Blog('My Developer Journey', 'A personal story about how I chose to become a developer', 'general', '../img/self.png', 'blog/My Developer Journey.html')
-post2 = blog_object.Blog('3 Ways to Create React Components', 'A brief indtoduction on how to write components in React', 'javascript', '../img/reactjs.png', 'blog/3 Ways to Create React Components.html')
-post3 = blog_object.Blog('Making a Python Web Crawler', 'How I made a Python Web Crawler to automate a boring daily task', 'python', '../img/post2-0.png', 'blog/Making a Python Web Crawler.html')
+personal_story = blog_object.Blog('My Developer Journey', 'A personal story about how I chose to become a developer', 'general', 'img/self.png', 'blog/My Developer Journey.html')
+post2 = blog_object.Blog('3 Ways to Create React Components', 'A brief indtoduction on how to write components in React', 'javascript', 'img/reactjs.png', 'blog/3 Ways to Create React Components.html')
+post3 = blog_object.Blog('Making a Python Web Crawler', 'How I made a Python Web Crawler to automate a boring daily task', 'python', 'img/post2-0.png', 'blog/Making a Python Web Crawler.html')
 blog_list = [personal_story, post2, post3]
 
 blog.open_blog(blog_list)
