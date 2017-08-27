1d2d484e1347d8790d2b2491aff498642d553d8c
Banhawy <the.benhawy@gmail.com>
2017-08-26 20:11:32 -0500

    Added url property to blog object

--- docs/python/blog_object.py
+++ docs/python/blog_object.py
@@ -1,7 +1,8 @@
 class Blog():
     """ This class provides a way of storing project related information"""
-    def __init__(self, title, description, topic, image):
+    def __init__(self, title, description, topic, image, url):
         self.title = title
         self.description = description
         self.image_path = image
         self.topic = topic
+        self.url = url
